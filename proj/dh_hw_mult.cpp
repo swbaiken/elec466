f5443df5a5a3a3f275441964a328acb175b15c5b
Shaun Aiken <swbaiken@uvic.ca>
2016-07-12 21:46:22 -0700

    no message

--- proj/dh_hw_mult.cpp
+++ proj/dh_hw_mult.cpp
@@ -12,46 +12,75 @@ void dh_hw_mult::do_mult() {
 	c = in_data_2.read();
 		
 	// Original code from NN_DigitMult()...		
-  	bHigh = (NN_HALF_DIGIT)HIGH_HALF (b);
-  	bLow = (NN_HALF_DIGIT)LOW_HALF (b);
-  	cHigh = (NN_HALF_DIGIT)HIGH_HALF (c);
-  	cLow = (NN_HALF_DIGIT)LOW_HALF (c);
+	bHigh = (NN_HALF_DIGIT)HIGH_HALF (b);
+	bLow = (NN_HALF_DIGIT)LOW_HALF (b);
+	cHigh = (NN_HALF_DIGIT)HIGH_HALF (c);
+	cLow = (NN_HALF_DIGIT)LOW_HALF (c);
 
-  	a[0] = (NN_DIGIT)bLow * (NN_DIGIT)cLow;
-  	t = (NN_DIGIT)bLow * (NN_DIGIT)cHigh;
-  	u = (NN_DIGIT)bHigh * (NN_DIGIT)cLow;
-  	a[1] = (NN_DIGIT)bHigh * (NN_DIGIT)cHigh;
-  
-  	if ((t += u) < u) a[1] += TO_HIGH_HALF (1);
-  	u = TO_HIGH_HALF (t);
-  
-  	if ((a[0] += u) < u) a[1]++;
-  	a[1] += HIGH_HALF (t);
+	a[0] = (NN_DIGIT)bLow * (NN_DIGIT)cLow;
+	t = (NN_DIGIT)bLow * (NN_DIGIT)cHigh;
+	u = (NN_DIGIT)bHigh * (NN_DIGIT)cLow;
+	a[1] = (NN_DIGIT)bHigh * (NN_DIGIT)cHigh;
+
+	if ((t += u) < u) a[1] += TO_HIGH_HALF (1);
+	u = TO_HIGH_HALF (t);
+
+	if ((a[0] += u) < u) a[1]++;
+	a[1] += HIGH_HALF (t);
 	
 	// Write outputs
 	out_data_low.write(a[0]);
 	out_data_high.write(a[1]);
 }
 
-void dh_hw_mult::process_hw_mult()
-{
+void dh_hw_mult::process_hw_mult() {
 
+}
+
+void dh_hw_mult::state_reg() {
+	for (;;) {
+		//If we want to add a reset
+		//if (reset.read() == SC_LOGIC_1) state.write(S0_BEGIN);
+		//else 
+		state.write(next_state.read());
+		wait();
+	}
+}
+
+void dh_hw_mult::state_transition() {
 	for (;;) {
   	  
 		wait();
 		
-		if (hw_mult_enable.read() == true) {
-			
-			do_mult();
-			
-			while(hw_mult_enable.read() == true) {
+		switch(state.read()) {
+			case S0_WAIT:
+				if (hw_mult_enable.read() == true) {
+					next_state.write(S1_EXECUTE);
+				}
+				else {
+					next_state.write(S0_WAIT);
+				}
+				break;
+			case S1_EXECUTE:
+				dh_hw_mult::do_mult();
+				next_state.write(S2_OUTPUT);
+				break;
+			case S2_OUTPUT:
+				// Extract output from do_mult?
 				hw_mult_done.write(true);
-				wait();
-			}
-			
-			hw_mult_done.write(false);
-			
+				next_state.write(S3_FINISH);
+				break;
+			case S3_FINISH:
+				if (hw_mult_enable.read() == true) {
+					next_state.write(S3_FINISH);
+				}
+				else {
+					hw_mult_done.write(false);
+					next_state.write(S0_WAIT);
+				}
+				break;
+			default:
+				next_state.write(S0_WAIT);
 		}
-		
 	}
 }
--- proj/dh_hw_mult.h
+++ proj/dh_hw_mult.h
@@ -4,22 +4,32 @@
 #ifndef _DH_HW_MULT_H_
 #define _DH_HW_MULT_H_ 1
 
+enum ctrl_state {S0_WAIT, S1_EXECUTE, S2_OUTPUT, S3_FINISH};
+
 SC_MODULE (dh_hw_mult)
 {
-  sc_in<bool> hw_mult_enable; 
-  sc_in<NN_DIGIT> in_data_1;
-  sc_in<NN_DIGIT> in_data_2;
-  sc_out<NN_DIGIT> out_data_low;
-  sc_out<NN_DIGIT> out_data_high;
-  sc_out<bool> hw_mult_done;
+	sc_in_clk			clk;
+	
+	sc_in<bool>			hw_mult_enable; 
+	sc_in<NN_DIGIT>	in_data_1;
+	sc_in<NN_DIGIT>	in_data_2;
 
-  void process_hw_mult();
-  
-  SC_CTOR (dh_hw_mult)
-  {
-    SC_THREAD (process_hw_mult);
-    sensitive << hw_mult_enable;
-  }
+	sc_out<NN_DIGIT>	out_data_low;
+	sc_out<NN_DIGIT>	out_data_high;
+	sc_out<bool>		hw_mult_done;
+	
+	sc_signal<ctrl_state>	state, next_state;
+
+	void process_hw_mult();
+	void state_transition();
+	void state_reg();
+
+	SC_CTOR (dh_hw_mult)
+	{
+		SC_CTHREAD(state_reg, clk.pos());
+		SC_METHOD(state_transition);
+			sensitive << state;
+	}
   
 };
 
