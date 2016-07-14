596f01814c4d804c75481be3781ec1fef308ed86
Shaun Aiken <swbaiken@uvic.ca>
2016-07-14 09:43:53 -0700

    the landslide of tiny edits beings

--- proj/dhdemo.cpp
+++ proj/dhdemo.cpp
@@ -18,20 +18,20 @@ int sc_main(int argc , char *argv[])
 	DH_SW.out_data_2 (operand2);		// operand2 to hardware
 	DH_SW.in_data_low (result1);		// result1 from hardware
 	DH_SW.in_data_high (result2);		// result2 from hardware
-	DH_SW.hw_mult_enable (enable);		// enable hardware
-	DH_SW.hw_mult_done (done);		// hardware done
+	DH_SW.hw_mult_enable (enable);	// enable hardware
+	DH_SW.hw_mult_done (done);			// hardware done
 	
 	
 	dh_hw_mult DH_HW_MULT("DH_Hardware_Multiplier");
 	DH_HW_MULT.clk(clock);
 	DH_HW_MULT.hw_mult_enable (enable);	// enable hardware
-	DH_HW_MULT.in_data_1 (operand1);	// operand1 from software
-	DH_HW_MULT.in_data_2 (operand2);	// operand2 from software
+	DH_HW_MULT.in_data_1 (operand1);		// operand1 from software
+	DH_HW_MULT.in_data_2 (operand2);		// operand2 from software
 	DH_HW_MULT.out_data_low (result1);	// result1 to software
 	DH_HW_MULT.out_data_high (result2);	// result2 to software
 	DH_HW_MULT.hw_mult_done (done);		// hardware done
 
-	sc_start();
+	sc_start(10,SC_SEC);						// limit total run time
 
 	return(0);
 }
