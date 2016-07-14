180deebe5235dd67f9505100d610d34a364ded3d
Shaun Aiken <swbaiken@uvic.ca>
2016-07-14 12:53:19 -0700

    no message

--- proj/dh_hw_mult.h
+++ proj/dh_hw_mult.h
@@ -29,7 +29,7 @@ SC_MODULE (dh_hw_mult)
 	{
 		SC_CTHREAD(state_reg, clk.pos());
 		SC_THREAD(state_transition);
-			sensitive << state << hw_mult_enable << in_data_1 << in_data_2;
+			sensitive << state << hw_mult_enable;// << in_data_1 << in_data_2;
 		
 		state.write(S98_INIT);
 		next_state.write(S98_INIT);
