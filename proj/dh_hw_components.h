eb3ec5c3b064f37e721abb31c3b2000e9d774f19
Shaun <swbaiken@users.noreply.github.com>
2016-07-22 14:40:44 -0700

    most of the way - port not bound now? why aren't labels used?

--- proj/dh_hw_components.h
+++ proj/dh_hw_components.h
@@ -2,11 +2,12 @@
 #define DH_HW_COMPONENTS_H
 
 #include "digit.h"
+#include "systemc.h"
 
 
 /*
  * COMPONENT
- */
+ *
 
 SC_MODULE (COMPONENT) {
 	sc_in<>	input;
@@ -21,6 +22,7 @@ SC_MODULE (COMPONENT) {
 				sensitive << input;
 	}
 };
+*/
 
 /*
  * Multiplier
@@ -30,14 +32,14 @@ SC_MODULE (multiplier) {
 	sc_in<NN_HALF_DIGIT>	in1;
 	sc_in<NN_HALF_DIGIT>	in2;
 	
-	sc_out<NN_DIGIT> product;
+	sc_out<NN_DIGIT> out;
 	
-	void func(void) {
-		product->write(in1->read() * in2->read());
+	void mult_func() {
+		out.write(in1.read() * in2.read());
 	}
 	
 	SC_CTOR(multiplier) {
-		SC_METHOD(func);
+		SC_METHOD(mult_func);
 			sensitive << in1 << in2;
 	}
 };
@@ -53,7 +55,7 @@ SC_MODULE (adder) {
 	sc_out<NN_DIGIT> sum;
 	
 	void func(void) {
-		sum->write(in1->read() + in2->read());
+		sum.write(in1.read() + in2.read());
 	}
 	
 	SC_CTOR(adder) {
@@ -72,7 +74,7 @@ SC_MODULE (half_shift_left) {
 	sc_out<NN_DIGIT>	output;
 	
 	void func(void) {
-		output->write(input->read() << NN_HALF_DIGIT_BITS);
+		output.write(input.read() << NN_HALF_DIGIT_BITS);
 	}
 	
 	SC_CTOR(half_shift_left) {
@@ -91,7 +93,7 @@ SC_MODULE (half_shift_right) {
 	sc_out<NN_DIGIT>	output;
 	
 	void func(void) {
-		output->write(input->read() >> NN_HALF_DIGIT_BITS);
+		output.write(input.read() >> NN_HALF_DIGIT_BITS);
 	}
 	
 	SC_CTOR(half_shift_right) {
@@ -107,17 +109,17 @@ SC_MODULE (half_shift_right) {
  */
 
 SC_MODULE (const_mem) {
-	sc_in<uint<1> > 	sel;
+	sc_in<sc_uint<1> > 	sel;
 	
 	sc_out<NN_DIGIT>	output;
 	
 	void func(void) {
-		switch (input->read()) {
+		switch (sel.read()) {
 			case 0:
-				output->write(1 << NN_HALF_DIGIT_BITS);
+				output.write(1 << NN_HALF_DIGIT_BITS);
 				break;
 			case 1:
-				output->write(1);
+				output.write(1);
 				break;
 		}
 	}
@@ -135,25 +137,27 @@ SC_MODULE (const_mem) {
 SC_MODULE (mux_2) {
 	sc_in<NN_DIGIT>	input0;
 	sc_in<NN_DIGIT>	input1;
-	sc_in<uint<1> >	mux;
+	sc_in<sc_uint<1> >	mux;
 		
 	sc_out<NN_DIGIT>	output;
 	
 	void func(void) {
-		switch (mux->read()) {
+		switch (mux.read()) {
 			case 0:
-				output->write(input0->read());
+				output.write(input0.read());
 				break;
 			case 1:
-				output->write(input1->read());
+				output.write(input1.read());
 				break;
 		}
 	}
 	
-	SC_CTOR(mux_2) {
-			SC_METHOD(func);
-				sensitive << mux;
+	mux_2(sc_module_name name) : sc_module(name) {
+		SC_METHOD(func);
+			sensitive << input0 << input1 << mux;
 	}
+	
+	SC_HAS_PROCESS(mux_2);
 };
 
 /*
@@ -164,33 +168,32 @@ SC_MODULE (mux_3) {
 	sc_in<NN_DIGIT>	input0;
 	sc_in<NN_DIGIT>	input1;
 	sc_in<NN_DIGIT>	input2;
-	//sc_in<NN_DIGIT>	input3; // Not used
-	sc_in<uint<2> >	mux;
+	sc_in<sc_uint<2> >	mux;
 		
 	sc_out<NN_DIGIT>	output;
 	
 	void func(void) {
-		switch (mux->read()) {
+		switch (mux.read()) {
 			case 0:
-				output->write(input0->read());
+				output.write(input0.read());
 				break;
 			case 1:
-				output->write(input1->read());
+				output.write(input1.read());
 				break;
 			case 2:
-				output->write(input2->read());
+				output.write(input2.read());
 				break;
 			case 3:
 				// Fall through to default
 			default:
-				output->write(MAX_NN_DIGIT);
+				output.write(MAX_NN_DIGIT);
 				break;
 		}
 	}
 	
-	SC_CTOR(mux_2) {
+	SC_CTOR(mux_3) {
 			SC_METHOD(func);
-				sensitive << mux;
+				sensitive << input0 << input1 << input2 << mux;
 	}
 };
 
@@ -202,23 +205,44 @@ SC_MODULE (input_splitter) {
 	sc_in<NN_DIGIT>	in0;
 	sc_in<NN_DIGIT>	in1;
 	
-	sc_out<NN_DIGIT>	in0_low;
-	sc_out<NN_DIGIT>	in0_high;
-	sc_out<NN_DIGIT>	in1_low;
-	sc_out<NN_DIGIT>	in1_high;
+	sc_out<NN_HALF_DIGIT>	in0_low;
+	sc_out<NN_HALF_DIGIT>	in0_high;
+	sc_out<NN_HALF_DIGIT>	in1_low;
+	sc_out<NN_HALF_DIGIT>	in1_high;
 	
 	void func(void) {
-		in0_low->write(in0->read() & MAX_NN_HALF_DIGIT);
-		in0_high->write((in0->read() >> NN_HALF_DIGIT_BITS) & MAX_NN_HALF_DIGIT);
-		in1_low->write(in1->read() & MAX_NN_HALF_DIGIT);
-		in1_high->write((in1->read() >> NN_HALF_DIGIT_BITS) & MAX_NN_HALF_DIGIT);
+		in0_low.write(in0.read() & MAX_NN_HALF_DIGIT);
+		in0_high.write((in0.read() >> NN_HALF_DIGIT_BITS) & MAX_NN_HALF_DIGIT);
+		in1_low.write(in1.read() & MAX_NN_HALF_DIGIT);
+		in1_high.write((in1.read() >> NN_HALF_DIGIT_BITS) & MAX_NN_HALF_DIGIT);
 	}
 	
 	SC_CTOR(input_splitter) {
 			SC_METHOD(func);
-				sensitive << int1 << in0;
+				sensitive << in1 << in0;
 	}
 };
 
+/*
+ * Memory unit
+ */
+
+SC_MODULE (mem_unit) {
+	sc_in<NN_DIGIT>	input;
+	sc_in<bool>			enable;
+	
+	sc_out<NN_DIGIT>	output;
+	
+	void func(void) {
+		if (enable.read()) {
+			output.write(input.read());
+		}
+	}
+	
+	SC_CTOR(mem_unit) {
+			SC_METHOD(func);
+				sensitive << enable;
+	}
+};
 
 #endif
--- proj/dh_hw_mult.h
+++ proj/dh_hw_mult.h
@@ -1,13 +1,14 @@
-#include "systemc.h"
-#include "digit.h"
-
 #ifndef _DH_HW_MULT_H_
 #define _DH_HW_MULT_H_ 1
 
-enum ctrl_state {S0_WAIT, S1_EXECUTE, S2_OUTPUT, S3_FINISH, S98_INIT, S99_INIT};
+#include "systemc.h"
+#include "digit.h"
+#include "dh_hw_components.h"
 
-SC_MODULE (dh_hw_mult)
-{
+enum ctrl_states {S0_WAIT, S1_EXECUTE, S2_OUTPUT, S3_FINISH, S98_INIT, S99_INIT};
+enum mult_states {MS0_WAIT, MS1_RUN, MS2_DONE};
+
+SC_MODULE (dh_hw_mult) {
 	sc_in_clk			clk;
 	
 	sc_in<bool>			hw_mult_enable; 
@@ -18,21 +19,30 @@ SC_MODULE (dh_hw_mult)
 	sc_out<NN_DIGIT>	out_data_high;
 	sc_out<bool>		hw_mult_done;
 	
+	sc_signal<ctrl_states>	state, next_state;
+	sc_signal<mult_states>	mult_state;
+	
 	sc_signal<NN_HALF_DIGIT> in0_low, in0_high, in1_low, in1_high;
-	sc_signal<sc_uint<NN_DIGIT_BITS> > mult_a0_out, mult_a1_out, mult_u_out, mult_t_out;
+	sc_signal<NN_DIGIT> mult_a0_out, mult_a1_out, mult_u_out, mult_t_out;
 	sc_signal<sc_uint<1> > a0_in_mux, t_in_mux, u_in_mux;
 	sc_signal<sc_uint<1> > constants_sel;	
 	sc_signal<sc_uint<2> > a1_in_mux;
-	sc_signal<sc_uint<NN_DIGIT_BITS> > a0_mux_out, a1_mux_out, t_mux_out, u_mux_out;
-	sc_signal<sc_uint<NN_DIGIT_BITS> > a0_add_out, a1_add1_out, a1_add2_out, t_add_out;
-	sc_signal<sc_uint<NN_DIGIT_BITS> > a0_val, a1_val, u_val, t_val;
-	sc_signal<sc_uint<NN_DIGIT_BITS> > t_shift_left_out, t_shift_right_out;
-	sc_signal<sc_uint<NN_DIGIT_BITS> > constants_out;
+	sc_signal<NN_DIGIT> a0_mux_out, a1_mux_out, t_mux_out, u_mux_out;
+	sc_signal<NN_DIGIT> a0_add_out, a1_add1_out, a1_add2_out, t_add_out;
+	sc_signal<NN_DIGIT> a0_val, a1_val, u_val, t_val;
+	sc_signal<NN_DIGIT> t_shift_left_out, t_shift_right_out;
+	sc_signal<NN_DIGIT> constants_out;
+	sc_signal<NN_DIGIT>	a0_out, a1_out, u_out, t_out;
+	sc_signal<bool>		a0_en, a1_en, u_en, t_en;
+	
+	sc_signal<bool> mult_enable;
+	sc_signal<bool> mult_done;
 	
 	
 	multiplier 		mult_a0, mult_a1, mult_u, mult_t;
-	adder				a0_plus_u, a1_plus_shift_t, al_plus_const, t_plus_u;
+	adder				a0_plus_u, a1_plus_shift_t, a1_plus_const, t_plus_u;
 	const_mem		constants;
+	mem_unit			a0, a1, u, t;
 	mux_2				a0_mux, u_mux, t_mux;
 	mux_3				a1_mux;
 	half_shift_left	t_shift_left;
@@ -40,28 +50,66 @@ SC_MODULE (dh_hw_mult)
 	input_splitter	splitter;
 			
 	
-	sc_signal<ctrl_state>	state, next_state;
 
 	//void process_hw_mult();
 	void state_transition();
 	void state_reg();
 	void state_output();
 	void do_mult();
+	void multiplier_control();
 
-	SC_CTOR (dh_hw_mult)
-	{
+	SC_CTOR (dh_hw_mult) : 
+		mult_a0("mult_a0"), mult_a1("mult_a1"), mult_u("mult_u"), mult_t("mult_t"),
+		a0_plus_u("a0_plus_u"), a1_plus_shift_t("a1_plus_shift"), 
+		a1_plus_const("a1_plus_const"), t_plus_u("t_plus_u"), constants("constants"),
+		a0("a0"), a1("a1"), u("u"), t("t"), a0_mux("a0_mux"),
+		u_mux("u_mux"), t_mux("t_mux"), a1_mux("a1_mux"), t_shift_left("t_shift_left"), 
+		t_shift_right("t_shift_right"), splitter("splitter") {
+			
 		SC_CTHREAD(state_reg, clk.pos());
 		SC_THREAD(state_output);
 			sensitive << state;
 		SC_THREAD(state_transition);
 			sensitive << state << hw_mult_enable;// << in_data_1 << in_data_2;
+		SC_THREAD(multiplier_control);
+			sensitive << clk.neg();
 		
 		state.write(S98_INIT);
 		next_state.write(S98_INIT);
 		
-		// out_data_low.initialize(0);
-		// out_data_high.initialize(0);
-		// hw_mult_done.initialize(true);
+		a0_in_mux.write(0);
+		a1_in_mux.write(0);
+		t_in_mux.write(0);
+		u_in_mux.write(0);
+		
+		a0_en.write(0);
+		a1_en.write(0);
+		u_en.write(0);
+		t_en.write(0);
+		
+		constants_sel.write(0);
+		
+		out_data_low.write(0);
+		out_data_high.write(0);
+		hw_mult_done.write(false);
+		
+		a0.input(a0_mux_out);
+		a0.output(a0_out);
+		a0.output(out_data_low);
+		a0.enable(a0_en);
+		
+		a1.input(a1_mux_out);
+		a1.output(a1_out);
+		a1.output(out_data_high);
+		a1.enable(a1_en);
+		
+		u.input(u_mux_out);
+		u.output(u_out);
+		u.enable(u_en);
+		
+		t.input(t_mux_out);
+		t.output(t_out);
+		t.enable(t_en);
 		
 		splitter.in0(in_data_1);
 		splitter.in1(in_data_2);
@@ -70,36 +118,36 @@ SC_MODULE (dh_hw_mult)
 		splitter.in1_low(in1_low);
 		splitter.in1_high(in1_high);
 		
-		mult_a0.in1();
-		mult_a0.in2();
+		mult_a0.in1(in0_low);
+		mult_a0.in2(in1_low);
 		mult_a0.out(mult_a0_out);
 		
-		mult_a1.in1();
-		mult_a1.in2();
+		mult_a1.in1(in0_high);
+		mult_a1.in2(in1_high);
 		mult_a1.out(mult_a1_out);
 		
-		mult_u.in1();
-		mult_u.in2();
+		mult_u.in1(in0_high);
+		mult_u.in2(in1_low);
 		mult_u.out(mult_u_out);
 		
-		mult_t.in1();
-		mult_t.in2();
+		mult_t.in1(in0_low);
+		mult_t.in2(in1_high);
 		mult_t.out(mult_t_out);
 		
-		a0_plus_u.in1(a0_mux_out);
-		a0_plus_u.in2(u_mux_out);
+		a0_plus_u.in1(a0_out);
+		a0_plus_u.in2(u_out);
 		a0_plus_u.sum(a0_add_out);
 		
-		a1_plus_shift_t.in1(a1_mux_out);
+		a1_plus_const.in1(a1_out);
+		a1_plus_const.in2(constants_out);
+		a1_plus_const.sum(a1_add1_out);
+		
+		a1_plus_shift_t.in1(a1_out);
 		a1_plus_shift_t.in2(t_shift_right_out);
-		a1_plus_shift_t.sum(a1_add1_out);
+		a1_plus_shift_t.sum(a1_add2_out);
 		
-		al_plus_const.in1(a1_mux_out);
-		al_plus_const.in2(constants_out);
-		al_plus_const.sum(a1_add2_out);
-		
-		t_plus_u.in1(t_mux_out);
-		t_plus_u.in2(u_mux_out);
+		t_plus_u.in1(t_out);
+		t_plus_u.in2(u_out);
 		t_plus_u.sum(t_add_out);
 		
 		a0_mux.input0(mult_a0_out);
@@ -123,10 +171,10 @@ SC_MODULE (dh_hw_mult)
 		t_mux.mux(t_in_mux);
 		t_mux.output(t_mux_out);
 		
-		t_shift_left.input(t_mux_out);
+		t_shift_left.input(t_out);
 		t_shift_left.output(t_shift_left_out);
 		
-		t_shift_right.input(t_mux_out);
+		t_shift_right.input(t_out);
 		t_shift_right.output(t_shift_right_out);
 		
 		constants.sel(constants_sel);
